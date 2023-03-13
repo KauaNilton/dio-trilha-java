daad226906072f1963f3b935d2d524e2eb344185
perkles <kaua4nilton@gmail.com>
2023-03-13 18:37:18 -0300

    Criando um projeto SmartTv

---
+++ dio-trilha-java-basico/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+
+
+public class Usuario {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(50);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+    }
+}
